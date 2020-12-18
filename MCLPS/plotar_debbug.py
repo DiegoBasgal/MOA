bd72662ad02d3bd31c9e8ef124816221b759e604
Lucas Lavratti <lucas@ritmoenergia.com.br>
2020-12-18 18:06:14 -0300

    Alteração nos comportamentos, gráfico para verificação, bugfix

--- MCLPS/plotar_debbug.py
+++ MCLPS/plotar_debbug.py
@@ -7,31 +7,43 @@ fp.close()
 sleep(0.5)
 
 
-limite_t = 3
+limite_t = 24
 
 fig, ax1 = plt.subplots()
-ax1.set_xlabel('Tempo decorrido na simulação (horas)')
+
+ax1.set_xlabel('Tempo decorrido na simulação')
 ax1.set_ylabel('Nível montante (m)', color='blue')
+ax1.axis([0, limite_t, 642.95, 643.75])
+
+ax3 = ax1.twinx()
+ax3.set_ylabel('Vazão Afluente (m³/s)', color='Green')
+ax3.axis([0, limite_t, 0, 30])
+ax3.spines['right'].set_position(('outward', 60))
+ax3.xaxis.set_ticks([])
+
 ax2 = ax1.twinx()
 ax2.set_ylabel('Potências (MW)', color='Red')
-ax1.axis([0, limite_t, 642.95, 643.75])
 ax2.axis([0, limite_t, 0, 5])
-ax1.axhline(y=643.50, color="red", linestyle="--")
-ax1.axhline(y=643.25, color="black", linestyle=":")
-ax1.axhline(y=643.00, color="red", linestyle="--")
+
+
+
+ax1.axhline(y=643.50, color="black", linestyle="--")
+ax1.axhline(y=643.25, color="gray", linestyle=":")
+ax1.axhline(y=643.00, color="black", linestyle="--")
 plt.title("MOA - Simulação do comportamento")
+plt.xticks(range(0, limite_t+1, 5))
 
-
-fig.tight_layout()  # otherwise the right y-label is slightly clipped
+#fig.tight_layout()  # otherwise the right y-label is slightly clipped
 
 # segundos_simulados, q_afluente, nv_montante, pot_ug1, pot_ug2
 
 t_sim = [0]
 nv_m = [0]
+q_aflu = [0]
 pot1 = [0]
 pot2 = [0]
 pott = [0]
-
+setpoint = [0]
 
 primeira_vez = True
 
@@ -43,31 +55,35 @@ while True:
         fp.close()
         sleep(0.1)
     if aux:
-
-
         aux = aux.split()[:]
         for a in range(len(aux)):
             aux[a] = float(aux[a])
         t_sim.append(aux[0]/3600)
+        q_aflu.append(aux[1])
         nv_m.append(aux[2])
         pot1.append(aux[3])
         pot2.append(aux[4])
         pott.append(aux[3]+aux[4])
+        setpoint.append(aux[5])
 
-    if int(t_sim[-1]) >= limite_t:
-        ax1.axis([int(t_sim[-1])-limite_t, t_sim[-1], 642.95, 643.75])
+    # if t_sim[-1] >= limite_t:
+       # ax1.axis([t_sim[-1]-limite_t, t_sim[-1], 642.95, 643.75])
+       # ax2.axis([t_sim[-1]-limite_t, t_sim[-1], 0, 5])
+       # ax3.axis([t_sim[-1]-limite_t, t_sim[-1], 0, 30])
 
     linha1, = ax1.plot(t_sim, nv_m, label='Nível montante', color='blue')
+    linha5, = ax3.plot(t_sim, q_aflu, label='Afluente', color='green')
     linha2, = ax2.plot(t_sim, pot1, label='Potência UG1', color='orange', linestyle="--")
     linha3, = ax2.plot(t_sim, pot2, label='Potência UG2', color='yellow', linestyle="--")
     linha4, = ax2.plot(t_sim, pott, label='Potência total', color='red', linestyle=":")
+    linha6, = ax2.plot(t_sim, setpoint, label='SetPoint', color='purple', linestyle=":")
     if primeira_vez:
-        plt.legend(handles=[linha1, linha2, linha3, linha4], loc='upper center', bbox_to_anchor=(0.5, -0.05), ncol=4)
+        plt.legend(handles=[linha1, linha5, linha2, linha3, linha4, linha6], loc='upper center', bbox_to_anchor=(0.5, -0.05), ncol=3)
 
-    print(t_sim)
-    print("running...")
+    # print(t_sim)
+    # print("running...")
 
-    plt.pause(1)
+    plt.pause(0.05)
     primeira_vez = False
 
 plt.show()
--- MCLPS/simulador_clp.py
+++ MCLPS/simulador_clp.py
@@ -1,7 +1,6 @@
 # Import site é necessário para funcionar fora da IDE devido a necessidade de adicionar o diretório ao PATH
+import math
 import site
-from math import copysign
-
 site.addsitedir('..')
 
 from datetime import datetime
@@ -21,183 +20,153 @@ import usina
 # Constantes
 ESCALA_DE_TEMPO = 10
 
-# Globais
-q_afluente = 0
-nv_montante = 0
-pot_ug1 = 1
-pot_ug2 = 0
-comporta_flags = 0
-comporta_fechada = True
-comporta_p1 = False
-comporta_p2 = False
-comporta_p3 = False
-comporta_p4 = False
-comporta_aberta = False
+PLOTAR_GRAFICO_DEBBUG = True
 
 
 # +-------------------------------------------------------------------------------------------------------------------+
 # | Comportamento do reservatório                                                                                     |
 # +-------------------------------------------------------------------------------------------------------------------+
-def comportamento_reservatorio():
-
-    logging.info("Thread comportamento_reservatório iniciada")
-
-    global nv_montante
-    global q_afluente
-    volume = 192300
-    q_vert = 0
-    q_comp = 0
-    q_sani = 0
-    q_turb = 0
-    q_eflu = 0
-    q_liquida = 0
-    nv_montante = - 0.0000000002 * ((volume / 1000) ** 4) + 0.0000002 * ((volume / 1000) ** 3) - 0.0001 * (
-            (volume / 1000) ** 2) + 0.0331 * ((volume / 1000)) + 639.43
-    nv_montante = round(nv_montante, 2)
-
-    amostras = db.get_amostras_afluente()
-
-    while True:
-
-        logging.info("Simuladndo a partir do inpicio da tabela de afluentes")
-        logging.debug("T simulação (s); Afluente; Efluente; Turbinado; Nv_montante")
-        t0 = datetime.now()
-        segundos_simulados = 0
-        a = 0
-        while a in range(len(amostras) - 1):
-            segundos_reais = (datetime.now()-t0).total_seconds()
-            segundos_simulados = segundos_reais * ESCALA_DE_TEMPO
-            if (amostras[a][0] - amostras[0][0]).total_seconds() > segundos_simulados:
-                a += 1
-
-            # TODO usar q_afluente da tabela!
-            # q_afluente = amostras[a][1]
-            q_afluente = (DataBank.get_words(7, 1)[0]/1000)
-            q_vert = usina.q_vertimento(nv_montante)
-            q_comp = usina.q_comporta(comporta_fechada, comporta_p1, comporta_p2, comporta_p3, comporta_p4,
-                                      comporta_aberta, nv_montante)
-            q_sani = usina.q_sanitaria(nv_montante)
-            q_turb = usina.q_turbinada(pot_ug1, pot_ug2)
-            q_eflu = (q_vert + q_comp + q_sani + q_turb)
-            q_liquida = q_afluente - q_eflu
-            volume += q_liquida * ESCALA_DE_TEMPO
-            if volume < 0 : volume = 0
-            nv_montante = - 0.0000000002 * ((volume / 1000) ** 4) + 0.0000002 * ((volume / 1000) ** 3) - 0.0001 * ((volume / 1000) ** 2) + 0.0331 * (volume / 1000) + 639.43
-            nv_montante = round(nv_montante, 3)
-            sleep(0.1)
-
-            # para grafico de debbug
-            fp = open('debug.out', 'w+')
-            fp.write("{:15.0f} {:5.5f} {:5.5f} {:5.5f} {:5.5f}\n".format(segundos_simulados,  q_afluente, nv_montante, pot_ug1, pot_ug2))
-            fp.close()
-            logging.debug(("{:15.0f};{:5.5f};{:5.5f};{:5.5f};{:5.5f}\n".format(segundos_simulados,  q_afluente, nv_montante, pot_ug1, pot_ug2)).replace(".",","))
-            #segundos_reais = (datetime.now()-t0).total_seconds()
-            #print(segundos_simulados/segundos_reais)
-        logging.info("Final das amostras")
-
-
-# +-------------------------------------------------------------------------------------------------------------------+
-# | Comportamento do CLP                                                                                              |
-# +-------------------------------------------------------------------------------------------------------------------+
-def comportamento_clp():
-    global q_afluente
-    global nv_montante
-    global pot_ug1
-    global pot_ug2
-    global comporta_flags
-    global comporta_fechada
-    global comporta_p1
-    global comporta_p2
-    global comporta_p3
-    global comporta_p4
-    global comporta_aberta
-
-    logging.info("Thread comportamento_clp iniciada")
-    logging.info("Iniciando Servidor/Slave Modbus ")
-
-    ip = CLPconfig.SLAVE_IP
-    porta = CLPconfig.SLAVE_PORT
-    temporizador = CLPconfig.CLP_REFRESH_RATE
-    REGS = [0] * 7  # 16 registradores
-    '''
-        Detalhamento dos conteúdos dos registradores
+class ComportamentoReal:
     
-        REGS[0]:    nivel_montante  [620 + X mm]
-        REGS[1]:    potencia_ug1    [X kw]
-        REGS[2]:    FLAGS_ug1     [RAW]
-        REGS[3]:    potencia_ug2    [X kw]
-        REGS[4]:    FLAGS_ug2     [RAW]
-        REGS[5]:    comporta_value  [RAW]
-        REGS[6]:    comporta_flags  [0]:fechada
-                                    [1]:pos1
-                                    [2]:pos2
-                                    [3]:pos3
-                                    [4]:pos4
-                                    [5]:aberta)
-    '''
+    setpoint_ug1 = 0
+    setpoint_ug2 = 0
+    comp_flags = 0
+    comp_fechada = 0
+    comp_p1 = 0
+    comp_p2 = 0
+    comp_p3 = 0
+    comp_p4 = 0
+    comp_aberta = 0
+    nv_montante = 0
+    volume = 0
+    pot_ug1 = 0
+    pot_ug2 = 0
+    sinc_ug1 = 0
+    sinc_ug2 = 0
 
-    server = ModbusServer(host=ip, port=porta, no_block=True)
-    try:
+    def __init__(self):
 
-        server.start()
+        self.volume = 192300
+        self.atualiza_nv_montante()
+        self.pot_ug1 = 0
+        self.pot_ug2 = 0
+        self.setpoint_ug1 = 0
+        self.setpoint_ug2 = 0
+        self.sinc_ug1 = 0
+        self.sinc_ug2 = 0
+
+    def atualiza_nv_montante(self):
+        nv = - 0.0000000002 * ((self.volume / 1000) ** 4) + 0.0000002 * ((self.volume / 1000) ** 3) - 0.0001 * (
+                    (self.volume / 1000) ** 2) + 0.0331 * (self.volume / 1000) + 639.43
+        self.nv_montante = nv
+        return nv
+
+    def comportamento_reservatorio(self):
+
+        logging.info("Comportamento_reservatório rodando")
+        amostras = db.get_amostras_afluente()
 
         while True:
-            # Limpa a interface de linha
-            os.system('cls' if os.name == 'nt' else 'clear')
 
-            # Carrega os REGs, da memória, sem conectar.
-            REGS = DataBank.get_words(0, 10)
+            logging.info("Simuladndo a partir do inicio da tabela de afluentes")
+            logging.debug("T simulação (s); Afluente; Efluente; Turbinado; Nv_montante")
 
-            # Entradas da CLP
-            pot_ug1 = (pot_ug1*14 + (int(REGS[1])/1000))/15
-            pot_ug2 = (pot_ug2*14 + (int(REGS[3])/1000))/15
+            t0 = datetime.now()
+            t1 = datetime.now()
+            segundos_simulados = 0
+            segundos_simulados_ant = 0
+            a = 0
+            while a in range(len(amostras) - 1):
 
-            comporta_flags = REGS[5]
-            comporta_fechada = REGS[6] & 0b00000001
-            comporta_p1 = REGS[6] & 0b00000010
-            comporta_p2 = REGS[6] & 0b00000100
-            comporta_p3 = REGS[6] & 0b00001000
-            comporta_p4 = REGS[6] & 0b00010000
-            comporta_aberta = REGS[6] & 0b00100000
-            q_afluente = REGS[7]/1000
+                segundos_reais = (datetime.now()-t0).total_seconds()
+                segundos_simulados_ant = segundos_simulados
+                segundos_simulados = segundos_reais * ESCALA_DE_TEMPO
+                delta_t_sim = segundos_simulados - segundos_simulados_ant
+                sleep(0.0001)  # tick
 
-            # Saidas da CLP
-            DataBank.set_words(0, [int((nv_montante - 620) * 1000)])
-            DataBank.set_words(7, [int(q_afluente * 1000)])
+                if (amostras[a][0] - amostras[0][0]).total_seconds() < segundos_simulados:
+                    a += 1
+                    t_c = (datetime.now() - t1).total_seconds()
+                    t_a = (amostras[a + 1][0] - amostras[a][0]).total_seconds()
+                    if t_c > 0:
+                        logging.debug("Escala da simulação: {:2.3f}".format(t_a / t_c))
+                    t1 = datetime.now()
 
-            print("###########\n"
-                  "#  PyCLP  #\n"
-                  "###########\n\n"
-                  "Servidor/Slave ModbusTCP em {}:{}\n\n"
-                  "TIME {}\n\n"
-                  "REG0  {: >16} | nivel_montante (-620m + Xmm)\n"
-                  "REG1  {: >16} | potencia_ug1 (kW)\n"
-                  "REG2  {:016b} | FLAGS_ug1 (BIN)\n"
-                  "REG3  {: >16} | potencia_ug2 (kW)\n"
-                  "REG4  {:016b} | FLAGS_ug2 (BIN)\n"
-                  "REG5  {: >16} | comporta_value (RAW)\n"
-                  "REG6  {:016b} | comporta_flags ([fechada][p1]..[p5][aberta])\n".format(ip, porta, datetime.now(),
-                                                                                          *REGS))
+                else:
 
-            sleep(temporizador)
-    finally:
-        pass
+                    # Acerta as UGS
 
-    logging.info("Thread comportamento_clp chegou ao final")
+                    #ug1
+                    if self.setpoint_ug1 >= 1:
+                        self.sinc_ug1 += 0.2 * (delta_t_sim / 60)
+                        if self.sinc_ug1 >= 1:
+                            self.sinc_ug1 = 1
+                            var_ug1_por_minuto = (self.setpoint_ug1 - self.pot_ug1) / (delta_t_sim / 60)
+                            var_ug1_por_minuto = math.copysign(min(0.625, abs(var_ug1_por_minuto)), var_ug1_por_minuto)
+                            self.pot_ug1 += (var_ug1_por_minuto / 60) * delta_t_sim
+                            self.pot_ug1 = max(1, self.pot_ug1)
+                    else:
+                        self.sinc_ug1 = 0
+                        self.pot_ug1 -= (0.625 / 60) * delta_t_sim
+                        self.pot_ug1 = max(0, self.pot_ug1)
+           
+                    #ug2
+                    if self.setpoint_ug2 >= 1:
+                        self.sinc_ug2 += 0.2 * (delta_t_sim / 60)
+                        if self.sinc_ug2 >= 1:
+                            self.sinc_ug2 = 1
+                            var_ug2_por_minuto = (self.setpoint_ug2 - self.pot_ug2) / (delta_t_sim / 60)
+                            var_ug2_por_minuto = math.copysign(min(0.625, abs(var_ug2_por_minuto)), var_ug2_por_minuto)
+                            self.pot_ug2 += (var_ug2_por_minuto / 60) * delta_t_sim
+                            self.pot_ug2 = max(1, self.pot_ug2)
+                    else:
+                        self.sinc_ug2 = 0
+                        self.pot_ug2 -= (0.625 / 60) * delta_t_sim
+                        self.pot_ug2 = max(0, self.pot_ug2)
+
+                    # Acerta as Vazoes
+                    q_aflu = amostras[a][1]
+                    q_vert = usina.q_vertimento(self.nv_montante)
+                    q_comp = usina.q_comporta(self.comp_fechada, self.comp_p1, self.comp_p2, self.comp_p3, self.comp_p4,
+                                              self.comp_aberta, self.nv_montante)
+                    q_sani = usina.q_sanitaria(self.nv_montante)
+                    q_turb = usina.q_turbinada(self.pot_ug1, self.pot_ug2)
+                    q_eflu = q_vert + q_comp + q_sani + q_turb
+                    q_liquida = q_aflu - q_eflu
+
+                    # Acerta o NV
+                    self.volume += q_liquida * delta_t_sim
+                    if self.volume < 0:
+                        self.volume = 0
+                    self.atualiza_nv_montante()
+
+                    # print("Simulando...")
+
+                    # para grafico de debbug
+                    if PLOTAR_GRAFICO_DEBBUG:
+                        fp = open('debug.out', 'w+')
+                        fp.write("{:15.0f} {:5.5f} {:5.5f} {:5.5f} {:5.5f} {:5.5f}\n".format(segundos_simulados,  q_aflu, self.nv_montante, self.pot_ug1,
+                                                                                     self.pot_ug2, self.setpoint_ug1+self.setpoint_ug2))
+                        fp.close()
+
+                    logging.debug(("{:15.0f};{:5.5f};{:5.5f};{:5.5f};{:5.5f};{:5.5f}\n".format(segundos_simulados,  q_aflu, self.nv_montante, self.pot_ug1,
+                                                                                     self.pot_ug2, self.setpoint_ug1+self.setpoint_ug2)))
+            logging.info("Final das amostras")
 
 
 def plotar_debug():
-    sleep(5)
     from MCLPS import plotar_debbug
+
 
 # +-------------------------------------------------------------------------------------------------------------------+
 # | MAIN                                                                                                              |
 # +-------------------------------------------------------------------------------------------------------------------+
 if __name__ == "__main__":
+
     # Inicializando o logging
     logFormatter = logging.Formatter("%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
     rootLogger = logging.getLogger()
-    rootLogger.setLevel(logging.DEBUG)
+    rootLogger.setLevel(logging.INFO)
     # LOG to file
     fileHandler = logging.FileHandler("simulador_clp.log", mode='w+')
     fileHandler.setFormatter(logFormatter)
@@ -207,21 +176,83 @@ if __name__ == "__main__":
     consoleHandler.setFormatter(logFormatter)
     rootLogger.addHandler(consoleHandler)
 
+    if PLOTAR_GRAFICO_DEBBUG:
+        t_plotar_debug = threading.Thread(target=plotar_debug)
+        t_plotar_debug.daemon = False
+        t_plotar_debug.start()
+        sleep(2)
+
     # Inicialização dos comportamentos
-    t_plotar_debug = threading.Thread(target=plotar_debug)
-    t_plotar_debug.daemon = False
-    t_plotar_debug.start()
-    sleep(5)
-
-
     logging.info("Simulador iniciado. Iniciando Threads")
 
-    t_comportamento_clp = threading.Thread(target=comportamento_clp)
-    t_comportamento_clp.daemon = False
-    t_comportamento_clp.start()
-
-    t_comportamento_reservatorio = threading.Thread(target=comportamento_reservatorio)
+    comportamento_usina = ComportamentoReal()
+    t_comportamento_reservatorio = threading.Thread(target=comportamento_usina.comportamento_reservatorio)
     t_comportamento_reservatorio.daemon = False
     t_comportamento_reservatorio.start()
+
+
+    ########################
+    # Comportamento da CLP #
+    ########################
+
+    logging.info("Iniciando Comportamento do clp")
+
+    logging.info("Iniciando Servidor/Slave Modbus ")
+    ip = CLPconfig.SLAVE_IP
+    porta = CLPconfig.SLAVE_PORT
+    temporizador = CLPconfig.CLP_REFRESH_RATE
+    REGS = [0] * 7  # 16 registradores
+    '''
+        Detalhamento dos conteúdos dos registradores
+
+        REGS[0]:    nivel_montante  [620 + X mm]
+        REGS[1]:    setpoint ug1    [X kw]
+        REGS[2]:    FLAGS_ug1     [RAW]
+        REGS[3]:    setpoint ug2    [X kw]
+        REGS[4]:    FLAGS_ug2     [RAW]
+        REGS[5]:    comp_value  [RAW]
+        REGS[6]:    comp_flags  [0]:fechada
+                                    [1]:pos1
+                                    [2]:pos2
+                                    [3]:pos3
+                                    [4]:pos4
+                                    [5]:aberta)
+        8 Pot REAL UG1
+        9 Pot REAL UG2
+
+    '''
+
+    server = ModbusServer(host=ip, port=porta, no_block=True)
+    try:
+        server.start()
+        while True:
+            # Limpa a interface de linha
+            os.system('cls' if os.name == 'nt' else 'clear')
+            print("CLP rodando...")
+            # Carrega os REGs, da memória, sem conectar.
+            REGS = DataBank.get_words(0, 10)
+
+            # Entradas da CLP
+            comportamento_usina.setpoint_ug1 = (int(REGS[1])/1000)
+            comportamento_usina.setpoint_ug2 = (int(REGS[3])/1000)
+            comportamento_usina.comp_flags = REGS[5]
+            comportamento_usina.comp_fechada = REGS[6] & 0b00000001
+            comportamento_usina.comp_p1 = REGS[6] & 0b00000010
+            comportamento_usina.comp_p2 = REGS[6] & 0b00000100
+            comportamento_usina.comp_p3 = REGS[6] & 0b00001000
+            comportamento_usina.comp_p4 = REGS[6] & 0b00010000
+            comportamento_usina.comp_aberta = REGS[6] & 0b00100000
+
+            # Saidas da CLP
+            DataBank.set_words(0, [int((comportamento_usina.nv_montante - 620) * 100) * 10])
+            DataBank.set_words(8, [int(comportamento_usina.pot_ug1 * 1000)])
+            DataBank.set_words(9, [int(comportamento_usina.pot_ug2 * 1000)])
+            sleep(temporizador)
+
+    except Exception as e:
+        logging.error("Erro na execução do CLP: {}".format(e))
+    finally:
+        logging.info("O comportamento_clp parou")
+        pass
 
     logging.info("Final da Main")
