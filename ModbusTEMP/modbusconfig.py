9d63b087a239a8fa6255da3a356293af02acdefd
Lucas Lavratti <lucas@ritmoenergia.com.br>
2021-02-23 20:41:44 +0000

    Deleted .idea/inspectionProfiles/profiles_settings.xml, .idea/.gitignore, .idea/MOA.iml, .idea/misc.xml, .idea/modules.xml, .idea/vcs.xml, ModbusTEMP/log.csv, ModbusTEMP/modbusconfig.py, ModbusTEMP/testeModbusClient.py, ModbusTEMP/testeModbusServer.py files

--- ModbusTEMP/modbusconfig.py
+++
@@ -1,4 +0,0 @@
-SLAVE_IP = "172.21.15.13"   # ip da máquina que vai rodar o slave
-SLAVE_PORT = 502            # porta do slave na máquina
-CLP_REFRESH_RATE = 0.5      # tempo entre atualização da CLP em segundos
-DEFAULT_REFRESH_RATE = 0.1  # tempo entre atualização em segundos
