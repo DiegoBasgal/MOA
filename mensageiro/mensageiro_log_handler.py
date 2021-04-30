40c69a62f870afb18a43bee19587d04ab2612fa4
Lucas Lavratti <lucas@ritmoenergia.com.br>
2021-04-30 12:10:06 -0300

    Integração loggin e comunicação externa voip/telegram

---
+++ mensageiro/mensageiro_log_handler.py
@@ -0,0 +1,14 @@
+from logging import Handler
+from . import telegram_bot
+from . import voip
+
+class MensageiroHandler(Handler):
+
+    def emit(self, record):
+        log_entry = self.format(record)
+        telegram_bot.enviar_a_todos(log_entry)
+
+        if record.levelno >= 50:
+            voip.enviar_voz_teste()
+            print("Erro CRIT: acionando VOIP")
+        return True
