94a08ef376eeb780e70ddbaf0e5411b5b53b9ca4
Lucas Lavratti <lucas@ritmoenergia.com.br>
2021-05-13 16:03:23 -0300

    Documentação

--- src/mensageiro/mensageiro_log_handler.py
+++ src/mensageiro/mensageiro_log_handler.py
@@ -1,14 +1,35 @@
+"""
+mensageiro_log_handler.py
+
+Este módulo integra o loggin com o voip e com o telegram.
+A princípio pode-se substituir essa implementação por dois handlers
+saparados, um para voip e um para o telegram, mas por motivos de
+praticidade, foi feito em apenas um handler.
+"""
+
 from logging import Handler
 from . import telegram_bot
 from . import voip
 
 
 class MensageiroHandler(Handler):
+    """
+    Esta classe é o handler que é passado para o loggin e tem
+    apenas a função emit(), reposavel por emitir o log.
+    """
 
     def emit(self, record):
+        """
+        Esta função recebe o texto a ser logado, já formatado, e envia para
+        os dstinatários utilizando os módulos de voip e telegram.
+        :param record:
+        :return: True
+        """
+
         log_entry = self.format(record)
         telegram_bot.enviar_a_todos(log_entry)
 
+        # Só dipara torpedos de voz em caso CRITICO (levelno >= 50)
         if record.levelno >= 50:
             voip.enviar_voz_teste()
             print("Erro CRIT: acionando VOIP")
