0b0c119171414fe3a076b4118d51dea1d0fb0a55
lucas <lucas@ritmoenergia.com.br>
2021-09-21 10:25:34 -0300

    added

---
+++ test/test_comporta.py
@@ -0,0 +1,8 @@
+import unittest
+
+class TesteComportamentoDaComporta(unittest.TestCase):
+    def test_nivel_abaixo_do_reservatorio(self):
+        self.assertEqual(True, False)  # add assertion here
+
+if __name__ == '__main__':
+    unittest.main()
