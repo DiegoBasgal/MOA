a65c153b6d26b5b05ac0809ad8985e13cb648659
lucas <lucas@ritmoenergia.com.br>
2021-09-24 16:32:43 -0300

    Added Comments and Tests

--- test/test_sm.py
+++ test/test_sm.py
@@ -16,27 +16,72 @@ class TesteComportamentoSM(unittest.TestCase):
         logging.disable(logging.NOTSET)
 
     def test_entra_no_estado(self):
-        estado = MagicMock()
-        parametros = MagicMock()
-        operador_autonomo_sm.StateMachine(estado(parametros))
-        estado.assert_called_once_with(parametros)
+        # Teste:            test_entra_no_estado
+        # Objetivo:         Verificar funcionamento basico da sm
+        # Resposta:         SM entra em um estado dado e executa o mesmo, devolvendo o proximo estado
 
-    def test_muda_de_estado(self):
-        estado = MagicMock()
+        estado = operador_autonomo_sm.State
+        estado.run = MagicMock()
+        estado.run.return_value = 'next_state'
         parametros = MagicMock()
-        estado2 = MagicMock()
-        estado.run.returns(estado2(parametros))
-        sm = operador_autonomo_sm.StateMachine(estado())
+        sm = operador_autonomo_sm.StateMachine(estado(parametros))
         sm.exec()
-        estado2.assert_called_once_with(parametros)
+        estado.run.assert_called_once()
+        self.assertEqual('next_state', sm.state)
+
+    def test_simple_sm(self):
+        # Teste:            test_entra_no_estado
+        # Objetivo:         Verificar funcionamento basico da sm
+        # Resposta:         SM alterna entre dois estados
+
+        class EstadoA:
+            def __init__(self, i):
+                self.i = i
+
+            def run(self):
+                return EstadoB(self.i+1)
+
+        class EstadoB:
+            def __init__(self, i):
+                self.i = i
+
+            def run(self):
+                return EstadoA(self.i + 1)
+
+        sm = operador_autonomo_sm.StateMachine(EstadoA(0))
+        for n in range(10):
+            sm.exec()
+            if n % 2:
+                self.assertIsInstance(sm.state, EstadoA)
+                self.assertNotIsInstance(sm.state, EstadoB)
+            else:
+                self.assertIsInstance(sm.state, EstadoB)
+                self.assertNotIsInstance(sm.state, EstadoA)
 
     def test_raise_into_falha_critica(self):
-        operador_autonomo_sm.FalhaCritica = MagicMock()
+        # Teste:            test_entra_no_estado
+        # Objetivo:         Verificar se a SM chama a Falha critica
+        # Resposta:         SM Falha e chama a Falha critica
         estado = MagicMock()
-        estado.run.side_effect = SystemError("test_raise_into_falha_critica")
+        estado.run.side_effect = TimeoutError
         sm = operador_autonomo_sm.StateMachine(estado)
         sm.exec()
-        operador_autonomo_sm.FalhaCritica.assert_called_once()
+        self.assertTrue(sm.em_falha_critica)
+        with self.assertRaises(SystemExit):
+            sm.exec()
+
+    def test_raise_into_falha_critica_2(self):
+
+        class EstadoA:
+            def run(self):
+                return 1/0
+
+        sm = operador_autonomo_sm.StateMachine(EstadoA())
+        sm.exec()
+
+        self.assertTrue(sm.em_falha_critica)
+        with self.assertRaises(SystemExit):
+            sm.exec()
 
 
 if __name__ == '__main__':
