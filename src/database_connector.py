d45b0ff669187b3f4301f83294cdc5444212e485
Lucas Lavratti <lucas@ritmoenergia.com.br>
2021-10-12 10:05:19 -0300

    Issue 65: Using prepared stataments

--- src/database_connector.py
+++ src/database_connector.py
@@ -7,7 +7,7 @@ https://stackoverflow.com/questions/38076220/python-mysqldb-connection-in-a-clas
 Here's some sample code where we create a table, add some data, and then read it back out:
 with Database('my_db.sqlite') as db:
     db.execute('CREATE TABLE comments(pkey INTEGER PRIMARY KEY AUTOINCREMENT, username VARCHAR, comment_body VARCHAR, date_posted TIMESTAMP)')
-    db.execute('INSERT INTO comments (username, comment_body, date_posted) VALUES (?, ?, current_date)', ('tom', 'this is a comment'))
+    db.execute('INSERT INTO comments (username, comment_body, date_posted) VALUES (%s, %s, current_date)', ('tom', 'this is a comment'))
     comments = db.query('SELECT * FROM comments')
     print(comments)
 """
@@ -70,55 +70,31 @@ class Database:
         return parametros
 
     def get_agendamentos_pendentes(self):
-        q = """SELECT id, DATE_SUB(data, INTERVAL 3 HOUR), comando_id, executado
-                         FROM agendamentos_agendamento
-                         WHERE executado = 0"""
+        q = "SELECT id, DATE_SUB(data, INTERVAL 3 HOUR), comando_id, executado " \
+            "FROM agendamentos_agendamento " \
+            "WHERE executado = 0;"
         return self.query(q)
 
     def update_parametrosusina(self, values):
-        q = """ UPDATE parametros_moa_parametrosusina
-                             SET
-                             timestamp = '{}',
-                             aguardando_reservatorio = {},
-                             clp_online = {},
-                             nv_montante = {},
-                             pot_disp = {},
-                             ug1_disp = {},
-                             ug1_pot = {},
-                             ug1_setpot = {},
-                             ug1_sinc = {},
-                             ug1_tempo = {},
-                             ug2_disp = {},
-                             ug2_pot = {},
-                             ug2_setpot = {},
-                             ug2_sinc = {},
-                             ug2_tempo = {},
-                             pos_comporta = {},
-                             ug1_perda_grade = {},
-                             ug1_temp_mancal = {},
-                             ug2_perda_grade = {},
-                             ug2_temp_mancal = {}        
-                             WHERE id = 1; 
-                             """.format(*values)
-        self.execute(q)
+        q = "UPDATE parametros_moa_parametrosusina " \
+            "SET timestamp = %s, aguardando_reservatorio = %s, clp_online = %s, nv_montante = %s, pot_disp = %s, " \
+            "ug1_disp = %s, ug1_pot = %s, ug1_setpot = %s, ug1_sinc = %s, ug1_tempo = %s, ug2_disp = %s, ug2_pot = %s, " \
+            "ug2_setpot = %s, ug2_sinc = %s, ug2_tempo = %s, pos_comporta = %s, ug1_perda_grade = %s, " \
+            "ug1_temp_mancal = %s, ug2_perda_grade = %s, ug2_temp_mancal = %s " \
+            "WHERE id = 1;"
+        self.execute(q, tuple(values))
         return True
 
     def update_agendamento(self, id_agendamento, executado):
-        if executado:
-            executado = 1
-        else:
-            executado = 0
+        executado = 1 if executado else 0
         q = "UPDATE agendamentos_agendamento " \
-            "SET executado = {} " \
-            "WHERE id = {}".format(executado, int(id_agendamento))
-        self.execute(q)
+            "SET executado = %s " \
+            "WHERE id = %s;"
+        self.execute(q, (executado, int(id_agendamento)))
 
     def update_emergencia(self, estado):
-        if estado:
-            estado = 1
-        else:
-            estado = 0
-        q = """UPDATE parametros_moa_parametrosusina
-                   SET emergencia_acionada = '{}'
-                   WHERE id = 1; """.format(estado)
-        self.execute(q)
+        estado = 1 if estado else 0
+        q = "UPDATE parametros_moa_parametrosusina " \
+            "SET emergencia_acionada = %s " \
+            "WHERE id = 1;"
+        self.execute(q, estado)
