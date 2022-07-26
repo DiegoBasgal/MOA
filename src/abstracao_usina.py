7f393713c630d7b72565e7559d395f06fe8af07f
Lucas Lavratti <lucas@ritmoenergia.com.br>
2022-07-26 11:35:08 -0300

    Issue 211

--- src/abstracao_usina.py
+++ src/abstracao_usina.py
@@ -1063,6 +1063,23 @@ class Usina:
         agora = datetime.now()
         agendamentos = self.get_agendamentos_pendentes()
 
+        # resolve os agendamentos muito juntos
+        limite_entre_agendamentos_iguais = 300 # segundos
+        agendamentos = sorted(agendamentos, key=lambda x:(x[3], x[1]))
+        i = 0
+        j = len(agendamentos)
+        while i < j - 1:
+
+            if agendamentos[i][3] == agendamentos[i+1][3] and (agendamentos[i+1][1] - agendamentos[i][1]).seconds < limite_entre_agendamentos_iguais:
+                ag_concatenado = agendamentos.pop(i)
+                obs = "Este agendamento foi concatenado ao seguinte por motivos de temporização."
+                logger.warning(obs)
+                self.db.update_agendamento(ag_concatenado[0], True, obs)
+                i -= 1
+
+            i += 1
+            j = len(agendamentos)
+
         logger.debug(agendamentos)
 
         if len(agendamentos) == 0:
