23e0d9c83db7b10345f52f0d5820788542b232e2
Diego Basgal Gasparoto Garcia <diego.garcia@ritmoenergia.com.br>
2023-08-08 18:17:00 -0300

    métodos de leitura e escrita melhorados com biblioteca pymodbus

--- src/funcoes/escrita.py
+++ src/funcoes/escrita.py
@@ -1,8 +1,8 @@
 import logging
 import traceback
 
-from time import sleep
-from pyModbusTCP.utils import *
+from pymodbus.constants import Endian
+from pymodbus.payload import BinaryPayloadDecoder as BPD
 from pyModbusTCP.client import ModbusClient
 
 logger = logging.getLogger("__main__")
@@ -10,38 +10,27 @@ logger = logging.getLogger("__main__")
 class EscritaModBusBit:
 
     @classmethod
-    def escrever_bit(cls, clp: "ModbusClient", reg: "int | list[int, int]", valor: "int", tamanho: int= 16, invertido: bool=False, descr: "str"=None) -> "bool":
+    def escrever_bit(cls, clp: "ModbusClient", reg: "list[int, int]", valor: "int") -> "bool":
         try:
-            lista_int = []
-            raw = clp.read_holding_registers(reg[0])[0]
+            raw = clp.read_holding_registers(reg[0])
+            raw_dec_1 = BPD.fromRegisters(raw, byteorder=Endian.Big, wordorder=Endian.Little)
+            raw_dec_2 = BPD.fromRegisters(raw, byteorder=Endian.Big, wordorder=Endian.Little)
 
-            if tamanho > 16:
-                if reg[1] < 16:
-                    raw_aux = clp.read_holding_registers(reg[0] + 1)[0]
-                    conv = get_bits_from_int(raw)
-                    conv_aux = get_bits_from_int(raw_aux)
-                    lista_bits = conv + conv_aux
+            if reg[1] >= 16:
+                raw_aux = clp.read_holding_registers(reg[0] + 1)
+                raw_aux_dec_1 = BPD.fromRegisters(raw_aux, byteorder=Endian.Big, wordorder=Endian.Little)
+                raw_aux_dec_2 = BPD.fromRegisters(raw_aux, byteorder=Endian.Big, wordorder=Endian.Little)
+                lista_int = [int(bit) for bits in [raw_dec_2.decode_bits(2), raw_dec_1.decode_bits(1), raw_aux_dec_2.decode_bits(2), raw_aux_dec_1.decode_bits(1)] for bit in bits]
 
-                elif reg[1] > 15:
-                    raw_aux = clp.read_holding_registers(reg[0] - 1)[0]
-                    conv = get_bits_from_int(raw)
-                    conv_aux = get_bits_from_int(raw_aux)
-                    lista_bits = conv_aux + conv
             else:
-                lista_bits = get_bits_from_int(raw)
-
-            for i in lista_bits:
-                aux = 1 if i else 0
-                lista_int.append(aux)
+                lista_int = [int(bit) for bits in [raw_dec_2.decode_bits(2), raw_dec_1.decode_bits(1)] for bit in bits]
 
             for i in range(len(lista_int)):
                 if reg[1] == i:
                     lista_int[i] = valor
                     break
-            if invertido:
-                v = sum(val*(2**x) for x, val in enumerate(reversed(lista_int)))
-            else:
-                v = sum(val*(2**x) for x, val in enumerate(lista_int))
+
+            v = sum(val*(2**x) for x, val in enumerate(lista_int))
 
             res = clp.write_single_register(reg[0], v)
             return res
--- src/funcoes/leitura.py
+++ src/funcoes/leitura.py
@@ -2,10 +2,9 @@ import logging
 import traceback
 
 from time import sleep
-from pyModbusTCP.utils import *
 from pyModbusTCP.client import ModbusClient
 from pymodbus.constants import Endian
-from pymodbus.payload import BinaryPayloadDecoder
+from pymodbus.payload import BinaryPayloadDecoder as BPD
 
 from src.dicionarios.const import *
 
@@ -92,7 +91,7 @@ class LeituraModbusCoil:
 
 
 class LeituraModbusBit(LeituraModbus):
-    def __init__(self, cli: "ModbusClient"=None, reg: "int | list[int, int]"=None, op: "int"=3, invertido: "bool"=None, descr: "str"=None) -> None:
+    def __init__(self, cli: "ModbusClient"=None, reg: "list[int, int]"=None, op: "int"=3, invertido: "bool"=None, descr: "str"=None) -> None:
         super().__init__(cli, reg, op, descr)
 
         self.__cli = cli
@@ -105,17 +104,18 @@ class LeituraModbusBit(LeituraModbus):
     @property
     def valor(self) -> "bool | None":
         try:
-            if self.__bit < 16:
-                raw_aux = self.__cli.read_holding_registers(self.__reg + 1)[0]
-                conv = get_bits_from_int(self.raw)
-                conv_aux = get_bits_from_int(raw_aux)
-                lista_bits = conv + conv_aux
+            raw = self.__cli.read_holding_registers(self.__reg)
+            raw_dec_1 = BPD.fromRegisters(raw, byteorder=Endian.Big, wordorder=Endian.Little)
+            raw_dec_2 = BPD.fromRegisters(raw, byteorder=Endian.Big, wordorder=Endian.Little)
 
-            elif self.__bit > 15:
-                raw_aux = self.__cli.read_holding_registers(self.__reg - 1)[0]
-                conv = get_bits_from_int(self.raw)
-                conv_aux = get_bits_from_int(raw_aux)
-                lista_bits = conv_aux + conv
+            if self.__bit >= 16:
+                raw_aux = self.__cli.read_holding_registers(self.__reg + 1)
+                raw_aux_dec_1 = BPD.fromRegisters(raw_aux, byteorder=Endian.Big, wordorder=Endian.Little)
+                raw_aux_dec_2 = BPD.fromRegisters(raw_aux, byteorder=Endian.Big, wordorder=Endian.Little)
+                lista_bits = [bit for bits in [raw_dec_2.decode_bits(2), raw_dec_1.decode_bits(1), raw_aux_dec_2.decode_bits(2), raw_aux_dec_1.decode_bits(1)] for bit in bits]
+
+            else:
+                lista_bits = [bit for bits in [raw_dec_2.decode_bits(2), raw_dec_1.decode_bits(1)] for bit in bits]
 
             for i in range(len(lista_bits)):
                 if self.__bit == i:
@@ -135,78 +135,16 @@ class LeituraModbusFloat(LeituraModbus):
         self.__reg = reg[0] if isinstance(reg, list) else reg
 
     @property
-    def raw_1(self) -> "int":
-        return self.__cli.read_holding_registers(self.__reg)[0]
-
-    @property
-    def raw_2(self) -> "int":
-        return self.__cli.read_holding_registers(self.__reg + 1)[0]
-
-    @property
     def valor(self) -> "int | float":
         try:
-            n = 16
-            aux = (self.raw_1 / (2 ** n))
+            raw = self.__cli.read_holding_registers(self.__reg)
 
-            bin_1 = str('{0:0b}'.format(self.raw_1))
-            bin_2 = '{0:0b}'.format(self.raw_2)
-
-            while aux < 1:
-                n = n - 1
-                aux = (self.raw_1 / (2 ** n))
-
-            n = n + 1
-
-            if (n) < 16:
-                for _ in range(16 - n):
-                    aux = bin_1
-                    bin_1 = '0' + aux
-
-            bin_conv = int((bin_2 + bin_1), 2)
-            ret = self.ieee_754_conversion(bin_conv, exp_len=8, mant_len=23)
-
-            return 0 if ret is None else ret
+            decoder = BPD.fromRegisters(raw, byteorder=Endian.Big, wordorder=Endian.Little)
+            
+            return decoder.decode_32bit_float()
 
         except Exception:
             logger.error(f"[LER] Houve um erro ao converter os valores Decimais para Float. Retornando 0.")
-            logger.debug(traceback.format_exc())
-            sleep(1)
-            return 0
-
-    def ieee_754_conversion(self, binary, sgn_len=1, exp_len=8, mant_len=23) -> "int | float":
-        try:
-            if binary >= 2 ** (sgn_len + exp_len + mant_len):
-                raise ValueError("[LER] O número binário é maior que o permitido, descrito nos parâmetros")
-
-            sign = (binary & (2 ** sgn_len - 1) * (2 ** (exp_len + mant_len))) >> (exp_len + mant_len)
-            exponent_raw = (binary & ((2 ** exp_len - 1) * (2 ** mant_len))) >> mant_len
-            mantissa = binary & (2 ** mant_len - 1)
-
-            sign_mult = 1
-            if sign == 1:
-                sign_mult = -1
-
-            if exponent_raw == 2 ** exp_len - 1:
-                if mantissa == 2 ** mant_len - 1:
-                    return float('nan')
-
-                return sign_mult * float('inf')
-
-            exponent = exponent_raw - (2 ** (exp_len - 1) - 1)
-
-            if exponent_raw == 0:
-                    mant_mult = 0
-            else:
-                mant_mult = 1
-
-            for b in range(mant_len - 1, -1, -1):
-                if mantissa & (2 ** b):
-                    mant_mult += 1 / (2 ** (mant_len - b))
-
-            return sign_mult * (2 ** exponent) * mant_mult
-
-        except Exception:
-            logger.error(f"[LER] Erro na conversão de binário para Float IEEE 754. Retornando 0.")
             logger.debug(traceback.format_exc())
             sleep(1)
             return 0
