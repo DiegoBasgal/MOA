dd5b0832587d10f783e2e5b064d5e50ea135f7b8
Diego Basgal <diego.garcia@ritmoenergia.com.br>
2023-11-14 17:53:20 -0300

    bugfix lógica de atenuação de carga

--- src/unidade_geracao.py
+++ src/unidade_geracao.py
@@ -885,7 +885,7 @@ class UnidadeGeracao:
         for condic in self.condicionadores_atenuadores:
             atenuacao = max(atenuacao, condic.valor)
             if self.etapa_atual == UG_SINCRONIZADA:
-                if atenuacao < 0:
+                if atenuacao > 0:
                     flags += 1
                     logger.debug(f"[UG{self.id}]          - \"{condic.descr}\":   Leitura: {condic.leitura} | Atenuação: {atenuacao}")
 
