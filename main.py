e40a34382a454af99b2b7d22972fc8b5bf3c42e7
root <diego.garcia@ritmoenergia.com.br>
2024-03-05 20:23:20 +0000

    checkpoint

--- main.py
+++ main.py
@@ -114,7 +114,7 @@ if __name__ == "__main__":
             logger.debug("-----------------------------------------------------------------")
             sm.exec()
 
-            if usn.estado_moa in (MOA_SM_CONTROLE_DADOS, MOA_SM_MODO_MANUAL):
+            if usn.estado_moa in (MOA_SM_CONTROLE_ESTADOS, MOA_SM_MODO_MANUAL):
                 t_restante = max(TEMPO_CICLO_TOTAL - (time() - t_i), 0) / ESCALA_DE_TEMPO
                 t_i = time()
 
